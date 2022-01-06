0ac1232c651f8974035cd1840f65c54d5cc3fbe5
Matheus Gomes <matheusgomes192@hotmail.com>
2022-01-06 12:56:45 -0300

    Criando diretório Components e criando primeiro Component Header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './globals.scss';
 
+import Header from './components/Header';
+
 export default function App() {
   return(
     <div>
-      
+      <Header />
     </div>
   )
 }
