aaf2244010741065226be798c0e2db2127ebcbed
Matheus Gomes <matheusgomes192@hotmail.com>
2022-01-07 17:52:01 -0300

    Adicionando component Menu e fazendo ele ficar fixo na esquerda ocupando toda a altura da tela

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import React from 'react';
 import './globals.scss';
 
 import Header from './components/Header';
+import Menu from './components/Menu';
 
 
 // <div>Some Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 
 export default function App() {
   return(
-    <div>
-      <Header />
+    <div className='app'>
+      <Menu />
+
+      <main>
+        <Header />
+      </main>
     </div>
   )
 }
