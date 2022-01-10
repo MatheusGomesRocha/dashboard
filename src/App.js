18f34f99c00a687812b750c26ea8ab516ecd92f2
Matheus Gomes <matheusgomes192@hotmail.com>
2022-01-10 13:08:47 -0300

    Criando arquivo para estilizar pagina de login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './globals.scss';
+import styles from './login.module.scss';
 
 import Header from './components/Header';
 import Menu from './components/Menu';
@@ -12,13 +13,38 @@ import MainScreen from './components/MainScreen';
 export default function App() {
   return(
     <div className='app'>
-      <Menu />
+      <form>
+        <h2>Criar nova conta</h2>
+
+        <div className={styles.goToLogin}>
+          <p>Já tem uma conta?</p>
+          <span>Login</span>
+        </div>
+
+        <div className={styles.inputArea}>
+          <label>CPF</label>
+          <input />
+        </div>
+
+        <div className={styles.inlineInput}>
+          <div className={styles.inputArea}>
+            <label>Primeiro nome</label>
+            <input />
+          </div>
+
+          <div className={styles.inputArea}>
+            <label>Segundo nome</label>
+            <input />
+          </div>
+        </div>
+      </form>
+      {/* <Menu />
 
       <main>
         <Header />
 
         <MainScreen />
-      </main>
+      </main> */}
     </div>
   )
 }
--- src/components/MainScreen/index.js
+++ src/components/MainScreen/index.js
@@ -5,6 +5,35 @@ import styles from './mainScreen.module.scss';
 export default function MainScreen () {
     return(
         <div className={styles.container}>
+            <section className={styles.leftContainer}>
+                <h2>Overview</h2>
+
+                <div className={styles.valueArea}>
+                    <div className={styles.valueItem}>
+                        <p>Total Profit</p>
+                        <div>
+                            <span className={styles.value}>R$ 35.457</span>
+                            <span className={styles.percent}>+ 4.85%</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.valueItem}>
+                        <p>Total Expenses</p>
+                        <div>
+                            <span className={styles.value}>R$ 25.156</span>
+                            <span className={styles.percent}>+ 4.85%</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.valueItem}>
+                        <p>New Users</p>
+                        <div>
+                            <span className={styles.value}>2.841</span>
+                            <span className={styles.percent}>- 4.85%</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
